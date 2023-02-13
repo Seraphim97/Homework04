97d9b2887fe439cad7b754a206e96c6e95aaa1a4
Seraphim97 <savichevserafim@gmail.com>
2023-02-13 22:43:40 +0200

    returned this code to main method

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,20 @@ public static void main(String[] args) {
         client.sayHello();
 
 
+
+
+    Client Ivanov = new Client("Ivanov Ivan", 233.33, 123456333);
+    Client Smirnov = new Client("Smirnov Alex", 3232.32, 654321123);
+    Client Sobolev = new Client("Sobolev Nikolai", 123456.01, 987123456);
+
+
+
+
+    String helloClient = Ivanov.sayHello() +", "+ Ivanov.name;
+
+    double balanceDifference = Ivanov.balanceDifference(Ivanov.balance, Smirnov.balance);
+
+
         String result;
         result = client.sayHello();
     System.out.println(client.sayHello());
@@ -24,21 +38,6 @@ public static void main(String[] args) {
     }
     // System.out.println(result);
 
-
-
-
-
-
-
-    Client Ivanov = new Client("Ivanov Ivan", 233.33, 123456333);
-    Client Smirnov = new Client("Smirnov Alex", 3232.32, 654321123);
-    Client Sobolev = new Client("Sobolev Nikolai", 123456.01, 987123456);
-
-
-
-    String helloClient = Ivanov.sayHello() +", "+ Ivanov.name;
-
-    double balanceDifference = Ivanov.balanceDifference(Ivanov.balance, Smirnov.balance);
 
     }
 
